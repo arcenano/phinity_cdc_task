4a0e57f61c74540534099154dd39c7e018a135e8
Adam <Mariano Arce>
2025-11-25 07:26:20 -0700

    changed to baseline sources

--- sources/cdc_arbiter_top.sv
+++ sources/cdc_arbiter_top.sv
@@ -24,42 +24,8 @@ module cdc_arbiter_top #(
   logic [N-1:0][WIDTH-1:0]  data_d_vec;
   logic [N-1:0]             ready_d_vec;
 
-  genvar i;
-  generate
-    for (i = 0; i < N; i++) begin : g_cdc
-      cdc #(
-        .WIDTH(WIDTH),
-        .DELAY(DELAY)
-      ) u_cdc (
-        .clk_s   (clk_s),
-        .reset_s (reset_s),
-        .valid_s (valid_s[i]),
-        .data_s  (data_s[i]),
-        .ready_s (ready_s[i]),
+  // Instantiate CDC modules
 
-        .clk_d   (clk_d),
-        .reset_d (reset_d),
-        .ready_d (ready_d_vec[i]),
-        .data_d  (data_d_vec[i]),
-        .valid_d (valid_d_vec[i])
-      );
-    end
-  endgenerate
-
-  priority_arbiter #(
-    .N    (N),
-    .WIDTH(WIDTH)
-  ) u_arb (
-    .clk       (clk_d),
-    .rst       (reset_d),
-
-    .valid_in  (valid_d_vec),
-    .data_in   (data_d_vec),
-    .ready_out (ready_d_vec),
-
-    .data_out  (data_out),
-    .valid_out (valid_out),
-    .ready_in  (ready_in)
-  );
+  // Instantiate priority arbiter
 
 endmodule
